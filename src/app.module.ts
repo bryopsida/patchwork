841df33902bf2ddcf09e54ac2de67609ac81f960
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-06-18 08:25:16 -0500

    add analyzer and patcher module stubs

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { HealthModule } from './health/health.module'
 import { LoggerModule } from 'nestjs-pino'
+import { AnalyzerModule } from './analyzer/analyzer.module';
+import { PatcherModule } from './patcher/patcher.module';
 @Module({
-  imports: [ConfigModule.forRoot(), LoggerModule.forRoot(), HealthModule],
+  imports: [ConfigModule.forRoot(), LoggerModule.forRoot(), HealthModule, AnalyzerModule, PatcherModule],
   controllers: [AppController],
   providers: [AppService],
 })
