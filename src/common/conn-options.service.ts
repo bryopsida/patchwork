e8a3508c532f3a5711c567c21f22acc9fcead305
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-06-18 16:31:08 -0500

    mount in .env secret to feed redis conn options

--- src/common/conn-options.service.ts
+++ src/common/conn-options.service.ts
@@ -1,21 +1,30 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { RedisOptions, Transport } from '@nestjs/microservices'
 import { MicroserviceHealthIndicatorOptions } from '@nestjs/terminus'
 import { config } from 'dotenv'
 
 // ensure process.env is hydrated
-config()
+config({
+  // items in .env take precedence
+  override: true,
+})
 
 @Injectable()
 export class ConnOptionsService {
+  private readonly logger = new Logger(ConnOptionsService.name)
+
   getRedisOptions(): RedisOptions & MicroserviceHealthIndicatorOptions {
+    const options = {
+      host: process.env.PATCHWORK_REDIS_HOST ?? 'localhost',
+      port: parseInt(process.env.PATCHWORK_REDIS_PORT ?? '6379'),
+      password: process.env.PATCHWORK_REDIS_PASSWORD ?? undefined,
+    }
+    this.logger.log(
+      `Using redis host: ${options.host} on port ${options.port} for connections options, source from process.env.PATCHWORK_REDIS_HOST=${process.env.PATCHWORK_REDIS_HOST}, process.env.PATCHWORK_REDIS_PORT=${process.env.PATCHWORK_REDIS_PORT}`
+    )
     return {
       transport: Transport.REDIS,
-      options: {
-        host: process.env.PATCHWORK_REDIS_HOST ?? 'localhost',
-        port: parseInt(process.env.PATCHWORK_REDIS_PORT ?? '6379'),
-        password: process.env.PATCHWORK_REDIS_PASSWORD ?? undefined,
-      },
+      options,
     }
   }
 }
